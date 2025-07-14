f20f220a5f70cb71dba4b1fca7c3b187a90bd5b9
Ilya Lazarev <ilya-lazarev3012@yandex.ru>
2025-07-14 19:15:15 +0300

    write a few tasks

--- main.go
+++ main.go
@@ -1,5 +1,9 @@
-package trainingTasks
+package main
+
+import (
+	"awesomeProject1/task3_interfaces"
+)
 
 func main() {
-
+	task3_interfaces.Task3()
 }
---
+++ task3_interfaces/task3.go
@@ -0,0 +1,72 @@
+package task3_interfaces
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"strconv"
+)
+
+type Shape interface {
+	Area() float64
+}
+
+type Rectangle struct {
+	width, height float64
+}
+
+type Circle struct {
+	radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+func (r Rectangle) Area() float64 {
+	return r.width * r.height
+}
+
+func getFloatFromConsole() float64 {
+	reader := bufio.NewReader(os.Stdin)
+	s, _ := reader.ReadString('\n')
+	s = s[:len(s)-1]
+	flt, err := strconv.ParseFloat(s, 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return flt
+}
+
+func Task3() {
+	var rectangle Rectangle
+	var circle Circle
+	reader := bufio.NewReader(os.Stdin)
+	count := 0
+	for count < 1 {
+		fmt.Println("Enter \"Rectangle\" or \"Circle\" or \"End\"(if you want to finalise program): ")
+		input, _ := reader.ReadString('\n')
+		input = input[:len(input)-1]
+		fmt.Println("You entered:", input)
+		if input == "Rectangle" {
+			fmt.Print("Enter \"width\": ")
+			rectangle.width = getFloatFromConsole()
+			fmt.Print("Enter \"height\": ")
+			rectangle.height = getFloatFromConsole()
+			fmt.Print("Area of rectangle with \"width\", \"height\": ", rectangle.Area(), "\n")
+		}
+		if input == "Circle" {
+			fmt.Print("Enter \"radius\": ")
+			circle.radius = getFloatFromConsole()
+			fmt.Print("Area of circle with \"radius\": ", circle.Area(), "\n")
+		}
+		if input == "End" {
+			count++
+		}
+		if input != "Rectangle" && input != "Circle" && input != "End" {
+			fmt.Println("Invalid input")
+		}
+	}
+}
