7dc47d4d189b47e9ed4d791dbe9b457bf0820994
Ilya Lazarev <ilya-lazarev3012@yandex.ru>
2025-07-17 17:34:05 +0300

    task7 update

--- task7_requestCaching/task7.go
+++ task7_requestCaching/task7.go
@@ -2,25 +2,39 @@ package task7_requestCaching
 
 import (
 	"fmt"
-	"math/rand/v2"
 	"time"
 )
 
-type Cache[T any] struct {
-	items T
+type Cache struct {
+	m map[int]int
 }
 
-func Task7() {
-	cache := Cache[int]{items: slowSquare(rand.IntN(100))}
-	fmt.Println(callForInt(cache))
+func NewCache() *Cache {
+	return &Cache{m: make(map[int]int)}
+}
+
+func (c *Cache) Call(n int) int {
+	if val, ok := c.m[n]; ok {
+		fmt.Printf("Cache hit for %d\n", n)
+		return val
+	}
+	fmt.Printf("Cache miss for %d\n", n)
+	result := slowSquare(n)
+	c.m[n] = result
+	return result
 }
 
 func slowSquare(n int) int {
 	time.Sleep(100 * time.Millisecond)
-	defer fmt.Println("slowSquare: ", n)
+	fmt.Println("slowSquare executed:", n)
 	return n * n
 }
 
-func callForInt(cache Cache[int]) any {
-	return cache.items
+func Task7() {
+	cache := NewCache()
+
+	fmt.Println("First call (slow):", cache.Call(5))
+	fmt.Println("Second call (fast):", cache.Call(5))
+	fmt.Println("Call with new arg:", cache.Call(8))
+	fmt.Println("Second call (fast):", cache.Call(8))
 }
